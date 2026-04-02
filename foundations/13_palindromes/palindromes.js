22d1cf5dbcff8eb28c9aea316f0dd220766a629b
Mat Bushell <mathew.bushell@gmail.com>
2026-04-01 21:33:28 -0400

    Assignments 9, 13, 14

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const lowerCaseStr = str.toLowerCase();
+    const arr = Array.from(lowerCaseStr);
+    const letters = arr.filter(i => /[a-z0-9]/i.test(i));
+    console.log(letters);
+    const lettersReversed = letters.toReversed() ;
+    const areSame = letters.every((value, index) => value === lettersReversed[index]);
+    return areSame;
 
 };
 
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(int) {
+    const arr = [0, 1];
+    for(let i = 0; i < int; i++){
+    const first = arr.at(i);
+    const second = arr.at(-1);
+    arr.push(first + second);
+  }
+  return arr.at(int);
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
