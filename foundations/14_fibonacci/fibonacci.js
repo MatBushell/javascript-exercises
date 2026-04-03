30d5ffe03f0540646aa1a6f10088aaa336bb0239
Mathew Bushell <80981032+MatBushell@users.noreply.github.com>
2026-04-02 21:04:23 -0400

    Completed fibonacci
    Multi-line description of commit,
    feel free to be detailed.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,11 +1,20 @@
 const fibonacci = function(int) {
+  if (int >= 0) {
+
     const arr = [0, 1];
+
     for(let i = 0; i < int; i++){
     const first = arr.at(i);
     const second = arr.at(-1);
     arr.push(first + second);
+    }
+    return arr.at(int);
+
+  } else {
+    return "OOPS";
   }
-  return arr.at(int);
+
+
 };
 
 
