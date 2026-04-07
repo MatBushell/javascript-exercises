98e3911fabd3e7ae568eac2e1f3c5882d48bfbb3
Mathew Bushell <80981032+MatBushell@users.noreply.github.com>
2026-04-07 12:06:49 -0400

    Found the Oldest
    Multi-line description of commit,
    feel free to be detailed.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    arr.forEach((person) => {
+      const currentYear = new Date().getFullYear();
+      if (!person.yearOfDeath) {
+        person.age = (currentYear - person.yearOfBirth)
+      } else {
+        person.age = (person.yearOfDeath -
+                      person.yearOfBirth)
+        
+      }
+
+  });
+  const maxAge = Math.max(...arr.map(person => person.age));
+  const oldestPerson = arr.filter(person => person.age === maxAge);
+  // return console.log(maxAge);
+  return oldestPerson[0];
+    
 
 };
 
